d9635115f9f3e4deacb4e5e8a01ae6bf16badbff
KrishMKS <marupaka.kumaraswamy001@gmail.com>
2017-06-15 10:19:58 +0530

    This is my first Program about how to create a GitHub profile and how to upload your projects into GitHub..

--- gitprojects1/src/gitprojects1/Sample1.java
+++ gitprojects1/src/gitprojects1/Sample1.java
@@ -6,7 +6,37 @@ public class Sample1
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello GitHub... ");
-		System.out.println("This is my first Project..");
+		System.out.println("This is my first Program about how to create a GitHub profile and how to upload your projects into GitHub..");
+		
+		System.out.println("-> Open any Browser ");
+		System.out.println("-> www.github.com ");
+		System.out.println("-> enter user name, mailID, Password ");
+		System.out.println("-> verify mail adress ");
+		System.out.println("-> click on '+' symbol(top right) ");
+		System.out.println("-> select new Repository ");
+		System.out.println("-> enter Repository name ");
+		System.out.println("-> click on 'clone&download' then copy url ");
+		
+		System.out.println("-> open 'ECLIPS' ");
+		System.out.println("-> create a new project ");
+		System.out.println("-> ckick on 'WINDOW'->Show View->other->gitrepositories");
+		System.out.println("-> next click on 'Clone_a_Gitrepository'->next->next ");
+		System.out.println("->Directory:'folder location in machine'  D:\\gitprojects ");
+		System.out.println("-> right click on PROJECT ");
+		System.out.println("-> select 'TEAM' ");
+		System.out.println("-> 'SHARE PROJECT' ");
+		System.out.println("-> enter repository url ");
+		System.out.println("-> finish ");
+
+		System.out.println("-> again right click on PROJECT ");
+		System.out.println("-> select 'TEAM' ");
+		System.out.println("-> 'commit' ");
+		System.out.println("-> git Stagging ");
+		System.out.println("-> drag changes ");
+		System.out.println("-> write a commit message ");
+		System.out.println("-> click on 'COMMIT AND PUSH ");
+		
+	
 	}
 
 }
