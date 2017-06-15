d36ab909defa6455e93a237d267ad2e2d95a7b75
KrishMKS <marupaka.kumaraswamy001@gmail.com>
2017-06-15 09:54:45 +0530

    Hiii... this is my first program in GitHub...

---
+++ gitprojects1/src/gitprojects1/Sample1.java
@@ -0,0 +1,12 @@
+package gitprojects1;
+
+public class Sample1
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello GitHub... ");
+		System.out.println("This is my first Project..");
+	}
+
+}
